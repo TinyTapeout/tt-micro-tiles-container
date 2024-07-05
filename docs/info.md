ef857eeae29c8a669fdb7b4290dd09ea30d411f7
Uri Shaked <uri@urishaked.com>
2024-07-05 23:47:56 +0300

    feat: initial micro tiles container PoC

--- docs/info.md
+++ docs/info.md
@@ -9,12 +9,9 @@ You can also include images in this folder and reference them in the markdown. E
 
 ## How it works
 
-Explain how your project works
+Combined 4 micro tile sized projects into a single Tiny Tapeout tile.
 
 ## How to test
 
-Explain how to use your project
+Use uio[1:0] to select the active micro-tile project.
 
-## External hardware
-
-List external hardware used in your project (e.g. PMOD, LED display, etc), if any
--- src/project.v
+++ src/project.v
@@ -5,7 +5,7 @@
 
 `default_nettype none
 
-module tt_um_example (
+module tt_um_micro_tiles_container (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
@@ -16,12 +16,43 @@ module tt_um_example (
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // All output pins must be assigned. If not used, assign to 0.
-  assign uo_out  = ui_in + uio_in;  // Example: ou_out is the sum of ui_in and uio_in
+  wire [1:0] sel = uio_in[1:0];
+
+  assign uo_out  = uo_out_proj[sel];
   assign uio_out = 0;
   assign uio_oe  = 0;
 
+  wire [7:0] uo_out_proj [3:0];
+
+  tt_um_micro_test proj1(
+    .rst_n(rst_n),
+    .clk(clk),
+    .ui_in(ui_in),
+    .uo_out(uo_out_proj[0])
+  );
+
+  tt_um_micro_test proj2(
+    .rst_n(rst_n),
+    .clk(clk),
+    .ui_in(ui_in),
+    .uo_out(uo_out_proj[1])
+  );
+
+  tt_um_micro_test proj3(
+    .rst_n(rst_n),
+    .clk(clk),
+    .ui_in(ui_in),
+    .uo_out(uo_out_proj[2])
+  );
+
+  tt_um_micro_test proj4(
+    .rst_n(rst_n),
+    .clk(clk),
+    .ui_in(ui_in),
+    .uo_out(uo_out_proj[3])
+  );
+
   // List all unused inputs to prevent warnings
-  wire _unused = &{ena, clk, rst_n, 1'b0};
+  wire _unused = &{ena, uio_in[7:2], 1'b0};
 
 endmodule
