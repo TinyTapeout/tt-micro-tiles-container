5514336f65713caf4691a3b642ab0dc7b3b1f18e
Uri Shaked <uri@urishaked.com>
2024-07-06 10:31:08 +0300

    fix: make linter happier

--- src/project.v
+++ src/project.v
@@ -25,30 +25,30 @@ module tt_um_micro_tiles_container (
   wire [7:0] uo_out_proj [3:0];
 
   tt_um_micro_test proj1(
-    .rst_n(sel == 0 ? rst_n : 0),
-    .clk(sel == 0 ? clk : 0),
-    .ui_in(sel == 0 ? ui_in : 0),
+    .rst_n(sel == 0 ? rst_n : 1'b0),
+    .clk(sel == 0 ? clk : 1'b0),
+    .ui_in(sel == 0 ? ui_in : 8'h00),
     .uo_out(uo_out_proj[0])
   );
 
   tt_um_micro_test proj2(
-    .rst_n(sel == 1 ? rst_n : 0),
-    .clk(sel == 1 ? clk : 0),
-    .ui_in(sel == 1 ? ui_in : 0),
+    .rst_n(sel == 1 ? rst_n : 1'b0),
+    .clk(sel == 1 ? clk : 1'b0),
+    .ui_in(sel == 1 ? ui_in : 8'h00),
     .uo_out(uo_out_proj[1])
   );
 
   tt_um_micro_test proj3(
-    .rst_n(sel == 2 ? rst_n : 0),
-    .clk(sel == 2 ? clk : 0),
-    .ui_in(sel == 2 ? ui_in : 0),
+    .rst_n(sel == 2 ? rst_n : 1'b0),
+    .clk(sel == 2 ? clk : 1'b0),
+    .ui_in(sel == 2 ? ui_in : 8'h00),
     .uo_out(uo_out_proj[2])
   );
 
   tt_um_micro_test proj4(
-    .rst_n(sel == 3 ? rst_n : 0),
-    .clk(sel == 3 ? clk : 0),
-    .ui_in(sel == 3 ? ui_in : 0),
+    .rst_n(sel == 3 ? rst_n : 1'b0),
+    .clk(sel == 3 ? clk : 1'b0),
+    .ui_in(sel == 3 ? ui_in : 8'h00),
     .uo_out(uo_out_proj[3])
   );
 
